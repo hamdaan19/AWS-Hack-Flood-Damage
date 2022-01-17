0fbf3c229e769cbaf06b10dcdef6f6c026ca9499
hamdaan19 <md.hamdaan19@gmail.com>
2022-01-17 20:56:47 +0530

    Added comments

--- scripts/gui_application.py
+++ scripts/gui_application.py
@@ -1,3 +1,8 @@
+#This file will contain the code for GUI applications
+# Authors:
+#   1. Mohammad Hamdaan (md.hamdaan19@gmail.com)
+#   2. Punitharaj (punitharaj212@gmail.com)
+
 from tkinter import *
 
 root = Tk()
