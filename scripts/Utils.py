05a39357bd5488c0dc47bc8bcbfad3043a0e7388
Hamdaan <md.hamdaan19@gmail.com>
2022-01-21 06:02:12 +0000

    Debugged the code

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -22,16 +22,18 @@ def display_mask(output): # takes a numpy array of dimensions: (height, width)
     return mask
 
 
-def get_rgb_segmented(image_array, label, fg_color=[255, 0, 0], bg_color=[0, 0, 0], save_img=False, filename="rgb_img.png"):
+def highlight_labels(image_array, label, fg_color=[255, 255, 0], bg_color=[0, 0, 0], save_img=False, filename="rgb_img.png"):
     '''
     Utility for converting a prediction array of dimentions (height, width, num_classes)
-    into an .png file with one label value color code. 
+    into an .png file with one label value color code. Initial name was get_rgb_segmented() 
     '''
-    img_arr = np.argmax(image_array, axis=-1)
-    img_arr = np.reshape(img_arr, (240, 320))
+    img_arr = np.argmax(image_array, axis=-1) # (h, w, classes) -> (h, w)
     img_arr = np.uint8(img_arr)
     
-    new_array = np.zeros((240, 320, 3), dtype=int)
+    h = img_arr.shape[0]
+    w = img_arr.shape[1]
+    
+    new_array = np.zeros((h, w, 3), dtype=int)
     
     for i in tqdm(range(img_arr.shape[0])):
         for j in range(img_arr.shape[1]):
@@ -44,7 +46,7 @@ def get_rgb_segmented(image_array, label, fg_color=[255, 0, 0], bg_color=[0, 0, 
     im = Image.fromarray(new_array)
     if save_img == True: im.save(filename)
     
-    return im
+    return im 
     
 def split_image(path, split_size=(240, 320), save=False): # split_size=(height, width)
     filename = os.path.basename(path)
@@ -58,8 +60,8 @@ def split_image(path, split_size=(240, 320), save=False): # split_size=(height, 
     i = 0
     j = 0
     
-    for yi in range(0, height, split_size[0]):
-        for xi in range(0, width, split_size[1]):
+    for i, yi in enumerate(range(0, height, split_size[0])):
+        for j, xi in enumerate(range(0, width, split_size[1])):
             box = (
                 xi,
                 yi,
@@ -71,11 +73,8 @@ def split_image(path, split_size=(240, 320), save=False): # split_size=(height, 
             if save == True:
                 out_path = os.path.join("../assets/output_dir", f"{name}_{i}_{j}{ext}")
                 current_img.save(out_path)
-            j += 1
             
         out_list.append(row_list)
         row_list = [] # Empytying the list
-        j = 0         # Reseting the variable
-        i += 1
         
     return out_list
