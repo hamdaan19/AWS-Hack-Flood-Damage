6400cd0d89534ce37fed7480a916722a6ee4f522
Hamdaan <md.hamdaan19@gmail.com>
2022-02-01 07:09:58 +0000

    Modified

--- scripts/make_predictions.py
+++
@@ -1,9 +0,0 @@
-from tensorflow import keras
-
-def make_predictions(image):
-    my_model = keras.models.load_model("vgg_classification_model")
-    image_np = np.array(image)
-    prediction = my_model.predict(x=image_np)
-    output = np.argmax(prediction, axis=1)
-    
-    return output
