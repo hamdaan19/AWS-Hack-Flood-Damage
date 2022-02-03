cef8902a26b78167768f072eb49813c41b2dea5e
Hamdaan <md.hamdaan19@gmail.com>
2022-02-03 10:14:58 +0000

    Added soft code

--- scripts/xception_classification_model.py
+++ scripts/xception_classification_model.py
@@ -2,11 +2,11 @@ from keras.layers import Input, Lambda, Dense, Flatten
 from tensorflow.keras.applications import Xception
 from keras.models import Model
 
-def get_xception_model()
+def get_xception_model(split_size=(128,128), in_c=3)
     X = Xception(
         include_top=False,
         weights="imagenet",
-        input_shape=(128, 128, 3)
+        input_shape=split_size+(in_c,),
     )
 
     x = Flatten()(X.output)
